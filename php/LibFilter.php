93e1a4159071188dfc2784739e1f6ab49b746a18
an9ryfr09 <an9ryfr09@gmail.com>
2013-12-15 16:37:21 +0800

    t

--- php/LibFilter.php
+++ php/LibFilter.php
@@ -1,11 +1,24 @@
 <?php
+/**
+ * @desc 数据过滤模块
+ * @author an9ryfr09 <an9ryfr09@gmail.com>
+ * @package LibFilter.php
+ * @version v1.0
+ */
+class LibFilter{
 
-class Filter{
-
+	/**
+	 * @const int FILTER_BOTH_SPACE
+	 * @const int FILTER_LFET_SPACE
+	 * @const int FILTER_RIGHT_SPACE
+	 */
 	const FILTER_BOTH_SPACE = 0;
 	const FILTER_LFET_SPACE = 1;
 	const FILTER_RIGHT_SPACE = 2;
 
+	/**
+	 * $_contents string 要过滤的字符串
+	 */
 	protected $_contents;
 
 	public function __construct( $contents = null ){
@@ -16,53 +29,119 @@ class Filter{
 		$this->_setContents( $contents );
 	}
 
+	/**
+	 * @desc 设置要过滤的字符串
+	 * @param string $contents
+	 * @return void
+	 */
 	protected function _setContents( $contents ){
-		if( isset($contents) && !empty( $contents )){
 			$this->_contents = $contents;
-		}
 	}
 
-	public function __call( $func, $args ){
-		$this->_op( $func, $args );
-	}
-
-	private function _recursive( $array ){
-		return array_pop( $array );
-	}
-
-	protected function op( ){
-		if( is_array( $this->_contents ) ){
-			$element = $this->_recursive( $this->_contents );
-			$this->__FUNCTION__( $element );
+	/**
+	 * @desc 获取当前字符串
+	 * @return string | null
+	 */
+	public function getContents(){
+		if( isset( $this->_contents ) && !empty( $this->_contents )){
+			return $this->_contents;
 		}
 		else{
-			call_user_func( $func, $args );;
+			return null;
 		}
 	}
 
-	public function getContents( $contents = null ){
-		if( isset( $contents ) && !empty( $contents ))
-			$this->_setContents( $contents );{
-		}
-		return $this->_contents;
-	}
+	/**
+	 * @过滤前后空格
+	 * @param enum(0, 1, 2) $type
+	 * @return $this
+	 */
+	public function filterSpace( $type = self::FILTER_BOTH_SPACE ){
 
-	public function filterChar( $ch = ' ', $type = self::FILTER_BOTH_SPACE, $contents = null ){
 		switch( $type ){
 			case self::FILTER_BOTH_SPACE:
-				$this->_contents = trim( $this->getContents($contents), $ch );
+				$this->_setContents( trim( $this->getContents() ) );
 				break;
 			case self::FILTER_LFET_SPACE:
-				$this->_contents = ltrim( $this->getContents($contents), $ch );
+				$this->_setContents( ltrim( $this->getContents() ) );
 				break;
 			case self::FILTER_RIGHT_SPACE:
-				$this->_contents = rtrim( $this->getContents($contents), $ch );
+				$this->_setContents( rtrim( $this->getContents() ) );
 				break;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @desc 过滤字符
+	 * @param string | array $ch
+	 * @return $this
+	 */
+	public function filterChar( $ch = ' ' ){
+
+		if( is_array($ch) ){
+			foreach( $ch as $c ){
+				$this->_setContents( str_replace( $c, '', $this->getContents() ) );
+			}
+		}
+		else{
+			$this->_setContents( str_replace( $ch, '', $this->getContents() ) );
 		}
 		return $this;
 	}
 
+	/**
+	 * @desc 过滤html标签
+	 * @return $this
+	 */
+	public function filterHtmlTags(){
+		$this->_setContents( strip_tags( $this->getContents() ) );
+		return $this;
+	}
+
+	/**
+	 * @desc 将html转换为特殊字符
+	 * @return $this
+	 */
+	private function transforHtmlSpecialChars(){
+		$this->_setContents( htmlspecialchars( $this->getContents() ) );
+		return $this;
+	}
+
+	/**
+	 * desc 将包含特殊字符的字符串转换为正常字符串
+	 * @return $this
+	 */
+	public function transSpecialCharsHtml(){
+		$this->_setContents( html_entity_decode( $this->getContents() ) );
+		return $this;
+	}
+
+	/**
+	 * @desc 为字符串/添加转义
+	 * return $this
+	 */
+	private function addSlashesF(){
+		$this->_setContents( addslashes( $this->getContents() ) );
+		return $this;
+	}
+
+	/**
+	 * @desc 过滤客户端数据
+	 * @return $this
+	 */
+	public function filterClientData(){
+
+		return $this->addSlashesF()
+				->transforHtmlSpecialChars();
+	}
+
+	/**
+	 * @desc 取得结果
+	 * @return $_contents
+	 */
 	public function getResults(){
-		return $this->_contents;
+		return $this->getContents();
 	}
 }
--- php/example.php
+++ php/example.php
@@ -4,8 +4,6 @@
  * ----------------------------------------
  */
 include './LibCookie.php';
-
-
 $cookie = new Cookie( '/', '.angryfrog.com', 3600 );
 $cookie->set('example', 'test_cookie');
 echo $cookie->get('example');
@@ -15,10 +13,21 @@ echo $cookie->get('example');
  * validate example
  * ----------------------------------------
  */
+include './LibValidate.php';
 $validate = new Validate( $s, true );
-
 echo $validate->checkEmpty()
 			->checkEmail()
 			->checkBothDifference( array($s2, $s3) )
 			->checkLength( $s2, 1, 4)
-			->getResult( Validate::MSG_FORMAT_JSON );
+			->getResult( YlmfValidate::MSG_FORMAT_JSON );
+
+
+/*
+ * Filter example
+ * ----------------------------------------
+ */
+include './LibFilter.php';
+$str = '</br>1，23232,3\'<a href="111">ssdsd</a>';
+$filter = new Filter( $str );
+echo $filter->filterClientData()
+		->getResults();
